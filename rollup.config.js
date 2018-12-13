21c4138e77efc1cd5b26336129b35e97301ad9cc
mgrisole <grisole.maxime@gmail.com>
2018-12-13 14:53:26 +0100

    Debug update

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,23 @@
-import babel from 'rollup-plugin-babel';
+ import babel from 'rollup-plugin-babel';
 import serve from 'rollup-plugin-serve';
 import commonjs from 'rollup-plugin-commonjs';
 import {terser} from "rollup-plugin-terser";
 import typescript from 'rollup-plugin-typescript2';
+
+let plugins = [
+    typescript(),
+    commonjs(),
+    babel({
+        exclude: 'node_modules/**',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+];
+
+plugins = [
+    ...plugins,
+    process.env.dev ? serve({ contentBase: ['dist', 'docs'] }) : terser()
+
+]
 
 export default [{
     input: './src/Typograph.ts',
@@ -10,16 +25,7 @@ export default [{
         file: 'dist/Typograph.js',
         name: 'Typograph',
         format: 'umd',
-        sourcemap: true
+        sourcemap: true,
     },
-    plugins: [
-        typescript(),
-        commonjs(),
-        // babel({
-        //     exclude: 'node_modules/**',
-        //     extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        // }),
-        terser(),
-        serve({ contentBase: ['dist', 'docs'] }),
-    ]
+    plugins,
 }]
